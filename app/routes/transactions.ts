b559f0b4774fd41a27d30f15571d0c442c923185
Renan Moreira Gomes <renangomes.es@gmail.com>
2023-02-07 12:59:16 -0300

    adding new get routes

--- app/routes/transactions.ts
+++ app/routes/transactions.ts
@@ -4,6 +4,47 @@ import crypto from 'node:crypto'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*')
+
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (request, reply) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions')
+      .select('*')
+      .where('id', id)
+      .first()
+
+    if (!transaction) {
+      return reply.status(404).send()
+    }
+
+    return {
+      transaction,
+    }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', {
+        as: 'amount',
+      })
+      .first()
+
+    return {
+      summary,
+    }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
